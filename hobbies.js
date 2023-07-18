e9def6f0b912fabcfb96c3113e8602066ed0b171
Hash-2704 <130900046+Hash-2704@users.noreply.github.com>
2023-07-18 23:48:46 +0530

    Add files via upload

---
+++ hobbies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+<div id="Hobbies">
+<div></div>
+    <div class="container">
+        <h1 class="subtitle">Hobbies</h1>
+        <ul>
+            <li> Listening to music</li>
+            <li> Tennis</li>
+            <li> Swimming</li>
+        </ul> 
+    </div>
+
+</div>
