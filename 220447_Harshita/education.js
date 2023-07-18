45b72dab36708999601f39fa19567dac50875056
Hash-2704 <130900046+Hash-2704@users.noreply.github.com>
2023-07-18 23:59:14 +0530

    Add files via upload

---
+++ 220447_Harshita/education.js
@@ -0,0 +1,35 @@
+import React from 'react.js'
+
+<div id="Education">
+    <div class="container">
+        <div class="row">
+            
+            <div class="about-col-1">
+            <h1 class="subtitle">My Education</h1>
+            
+            <p>
+                I'm Harshita, a 19 year currently in my first year, pursuing Btech in Aerospace Engineering at IIT Kanpur. 
+                I have always been fascinated by technology and learning the science behind everything.
+            </p>
+            <p>
+                 I come from Pune and I have completed my schooling from Wisdom World School- ICSE Pune (10th grade).
+            </p>
+            <p>
+                I have also studied in DY Patil International School, Antwep, Belgium during my middle school. 
+            </p>
+            <p>
+                My junior college, i.e 11th and 12th grade was done at Alard Public School, Hinjewadi, Pune. During this time, I also prepared for JEE at Bakliwal Tutorials.
+            </p>
+            <p>
+                I am excited about the endless possibilities that lie ahead in my engineering journey. I am looking forward to internships, research opportunities, and further specialization in areas like Coding, Finance, Consulting.
+            </p>
+            
+            
+            </div>
+            <div class="about-col-2">
+                <img src="./Hiya.jpeg" width="300" />
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ 220447_Harshita/form.js
@@ -0,0 +1,17 @@
+import React from 'react.js'
+
+<div id="Advice_Form">
+    <div class="container">
+    
+    <div class="contact-right">
+        <form>
+            <input type="text" name="Name" placeholder="Your Name" required/>
+            <input type="email" name="email" placeholder="Your email" required/>
+            <input type="text" name="contact number" placeholder="Contact Number" required/>
+            <textarea name="Message" placeholder="Your Message" rows="6"></textarea>
+            <button type="submit" class="btn">Submit</button>
+
+        </form>
+    </div>
+    </div>
+</div>
