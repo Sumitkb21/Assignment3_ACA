58719e3a0e5aaf8b4960b5170ea8db1385dd585a
Hash-2704 <130900046+Hash-2704@users.noreply.github.com>
2023-07-18 23:55:15 +0530

    Delete home.js

--- home.js
+++
@@ -1,6 +0,0 @@
-import React from "react.js"
-<div class="header-text">
-            
-            <h1> Hey! Welcome to my webpage. </h1>
-            <h3>  I am <span>Harshita Chhuttani</span>, an undergraduate at IITK. </h3>
-        </div>
