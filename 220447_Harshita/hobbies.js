a7344bc485945014ad2250ff437a68efc8b4f158
Hash-2704 <130900046+Hash-2704@users.noreply.github.com>
2023-07-19 09:18:50 +0530

    Update hobbies.js

--- 220447_Harshita/hobbies.js
+++ 220447_Harshita/hobbies.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+
+export default function hobbies(){
 <div id="Hobbies">
 <div></div>
     <div class="container">
@@ -12,3 +14,4 @@ import React from 'react';
     </div>
 
 </div>
+}
