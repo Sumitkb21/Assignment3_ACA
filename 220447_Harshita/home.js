4961eba8d05d62c5ea4e991aedf723975559331e
Hash-2704 <130900046+Hash-2704@users.noreply.github.com>
2023-07-19 09:20:47 +0530

    Update home.js

--- 220447_Harshita/home.js
+++ 220447_Harshita/home.js
@@ -1,6 +1,11 @@
 import React from "react.js"
+
+export default function home(){
 <div class="header-text">
             
             <h1> Hey! Welcome to my webpage. </h1>
             <h3>  I am <span>Harshita Chhuttani</span>, an undergraduate at IITK. </h3>
         </div>
+
+
+}
